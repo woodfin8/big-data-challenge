290e39489b59cfa00f1613f1edb515b4a764a395
woodfin8 <woodfin8@gmail.com>
2020-03-26 15:47:13 -0400

    updated SQL schema

--- SQL_schema/schema.sql
+++ SQL_schema/schema.sql
@@ -26,3 +26,32 @@ CREATE TABLE vine_table (
   total_votes INTEGER,
   vine TEXT
 );
+
+CREATE TABLE toy_review_id_table (
+  review_id TEXT PRIMARY KEY NOT NULL,
+  customer_id INTEGER,
+  product_id TEXT,
+  product_parent INTEGER,
+  date DATE -- this should be in the formate yyyy-mm-dd
+);
+
+-- This table will contain only unique values
+CREATE TABLE toy_products (
+  product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+  product_title TEXT
+);
+
+-- Customer table for toy data set
+CREATE TABLE toy_customers (
+  customer_id INT PRIMARY KEY NOT NULL UNIQUE,
+  customer_count INT
+);
+
+-- toy vine table
+CREATE TABLE toy_vine_table (
+  review_id TEXT PRIMARY KEY,
+  star_rating INTEGER,
+  helpful_votes INTEGER,
+  total_votes INTEGER,
+  vine TEXT
+);
